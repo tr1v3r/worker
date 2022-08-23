8c1b9c91b52e8ae88547393ab1aa42fbdb04e06f
r1v3r <churiver@outlook.com>
2022-08-23 23:35:50 +0800

    feat: init websocket

---
+++ main.go
@@ -0,0 +1,54 @@
+// Package main provides ...
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"github.com/riverchu/pkg/log"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/ws", WSHanlder())
+	r.Run(":6789")
+}
+
+var upgrader = websocket.Upgrader{}
+
+func WSHanlder() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+		if err != nil {
+			return
+		}
+		defer ws.Close()
+		for {
+			mt, message, err := ws.ReadMessage()
+			if err != nil {
+				log.Warn("read message fail: %s", err)
+				break
+			}
+
+			var resp string
+			switch string(message) {
+			case "ping":
+				resp = "pong"
+			default:
+				resp = string(message)
+			}
+
+			err = ws.WriteMessage(mt, []byte(resp))
+			if err != nil {
+				log.Warn("write message fail: %s", err)
+				break
+			}
+		}
+	}
+}
